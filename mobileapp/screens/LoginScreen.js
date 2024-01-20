ec4fd0a80f14e397c1b6a6f5c34f36a0ee75c390
Alzaahid <alzaahid@gmail.com>
2024-01-20 13:20:21 +0530

    added api for domain

--- mobileapp/screens/LoginScreen.js
+++ mobileapp/screens/LoginScreen.js
@@ -1,38 +1,46 @@
 // LoginScreen.js
-import React, { useState } from 'react';
-import { View, ImageBackground, StyleSheet, Alert } from 'react-native';
-import { Input, Button, Card } from 'react-native-elements';
+import React, { useState } from "react";
+import { View, ImageBackground, StyleSheet, Alert } from "react-native";
+import { Input, Button, Card } from "react-native-elements";
 
-const backgroundImage = require('../assets/bg.png'); // Adjust the path based on your file location
+const backgroundImage = require("../assets/bg.png"); // Adjust the path based on your file location
 
 const LoginScreen = ({ navigation }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('http://192.168.0.9:3000/api/login', {
-        method: 'POST',
+      const response = await fetch("http://192.168.0.9:3000/api/login", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ username, password }),
+
+        /*const response = await fetch('https://yourdomain.com/api/login', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ username, password }),
+});*/
       });
 
       const data = await response.json();
 
       if (response.ok) {
         // User successfully logged in
-        Alert.alert('Login Successful');
-        navigation.navigate('Dashboard');
+        Alert.alert("Login Successful");
+        navigation.navigate("Dashboard");
         // Navigate to the Dashboard or perform other actions.
         // navigation.navigate('Dashboard');
       } else {
         // Handle login failure
-        Alert.alert('Login Failed', data.error);
+        Alert.alert("Login Failed", data.error);
       }
     } catch (error) {
-      console.error('Error during login:', error);
+      console.error("Error during login:", error);
     }
   };
 
@@ -45,14 +53,14 @@ const LoginScreen = ({ navigation }) => {
             placeholder="Username"
             value={username}
             onChangeText={(text) => setUsername(text)}
-            leftIcon={{ type: 'font-awesome', name: 'user' }}
+            leftIcon={{ type: "font-awesome", name: "user" }}
           />
           <Input
             placeholder="Password"
             value={password}
             onChangeText={(text) => setPassword(text)}
             secureTextEntry
-            leftIcon={{ type: 'font-awesome', name: 'lock' }}
+            leftIcon={{ type: "font-awesome", name: "lock" }}
           />
           <Button title="Login" onPress={handleLogin} />
         </Card>
@@ -64,16 +72,16 @@ const LoginScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   backgroundImage: {
     flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
+    resizeMode: "cover",
+    justifyContent: "center",
   },
   cardContainer: {
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
     borderRadius: 10,
     padding: 20,
     width: 300,

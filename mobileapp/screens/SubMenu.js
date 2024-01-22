9685b56ec70888eb08a64260e63949d9180b7936
Alzaahid <alzaahid@gmail.com>
2024-01-23 01:03:43 +0530

    working close button

--- mobileapp/screens/SubMenu.js
+++ mobileapp/screens/SubMenu.js
@@ -4,15 +4,28 @@ import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
 
 const SubMenu = ({ onClose, onProfilePress, onLogoutPress }) => {
+  const color = 'yourColorValue'; // Replace with your color value
+  const size = 20; // Replace with your desired size value
+
   return (
     <View style={styles.subMenuContainer}>
       <TouchableOpacity onPress={onProfilePress}>
-        <Text style={styles.subMenuItem}>Profile</Text>
+        <Text style={styles.subMenuItem}>
+          <FontAwesome5 name="home" color={color} size={size} /> Profile
+        </Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={onLogoutPress}>
-        <Text style={styles.subMenuItem}>Logout</Text>
+        <Text style={styles.subMenuItem}>
+        <FontAwesome5 name="sign-out-alt" color={color} size={size} />Logout
+        </Text>
       </TouchableOpacity>
-   
+
+      {/* Testing button to trigger onClose directly */}
+      <TouchableOpacity onPress={onClose}>
+        <Text style={styles.closeButton}>
+        <FontAwesome5 name="times" color={color} size={size} />Close
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
